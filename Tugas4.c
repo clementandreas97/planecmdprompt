c03399f18b29581903d5821b1f8fd8f5e37c6fb3
albertusdj <albertusdjauharidjohan@gmail.com>
2018-02-11 16:27:39 +0700

    Plane can move forward with spinning rotor

--- Tugas4.c
+++ Tugas4.c
@@ -6,8 +6,8 @@
 #include <sys/mman.h>
 #include <sys/ioctl.h>
 #include <math.h>
-
-int MAX_VERTEX = 20;
+#include <limits.h>
+#include <time.h>
 
 int xResolution;
 int yResolution;
@@ -19,6 +19,11 @@ long int screensize = 0;
 char *fbp = 0;
 long int location = 0;
 int **a;
+int **copy_of_a;
+int **rotor;
+int **copyrotor;
+int **true_rotor;
+int **true_copyrotor;
 
 // Draw point
 
@@ -172,43 +177,139 @@ void scanLine(int n, int c1, int c2, int c3) {
     }
 }
 
-void dilatasi(int **a, int n, int c1, int c2, int c3, int factor, int centerX, int centerY, int *stillAvailable) {
-    int i;
-    for (i = 0; i < n; i++) {
-        if (a[i][0]*factor - centerX >= xResolution) {
-            *stillAvailable = 0;
-        } else {
-            a[i][0] = a[i][0] * factor - centerX;    
+void clear(int n){
+	int i,j,k,gd,gm,dy,dx;
+    int x,y,temp;
+    int xi[n];
+    float slope[n];
+
+    // Initiate Variables
+    a[n][0]=a[0][0];
+    a[n][1]=a[0][1];
+    
+    // Calculate slope
+    for(i=0;i<n;i++) {
+        dy=a[i+1][1]-a[i][1];
+        dx=a[i+1][0]-a[i][0];
+        if(dy==0) slope[i]=1.0;
+        if(dx==0) slope[i]=0.0;
+        if((dy!=0)&&(dx!=0)) {
+            slope[i]=(float) dx/dy;
         }
-        if (a[i][1]*factor - centerY >= yResolution) {
-            *stillAvailable = 0;
-        } else {
-            a[i][1] = a[i][1] * factor - centerY;
+    }
+
+    // Fill color
+    for(y=0;y<yResolution;y++) {
+        k=0;
+        // Initiate xi
+        for(i=0;i<n;i++) {
+            if (((a[i][1]<=y)&&(a[i+1][1]>y)) || ((a[i][1]>y)&&(a[i+1][1]<=y))) {
+                xi[k]=(int)(a[i][0]+slope[i]*(y-a[i][1]));
+                k++;
+            }
+        }
+        // Sort xi
+        for(j=0;j<k-1;j++)
+            for(i=0;i<k-1;i++) {
+                if(xi[i]>xi[i+1]) {
+                    temp=xi[i];
+                    xi[i]=xi[i+1];
+                    xi[i+1]=temp;
+                }
+            }
+        // Fill
+        for(i=0;i<k;i+=2) {
+            bresenham(xi[i],y,xi[i+1]+1,y,0,0,0);
         }
     }
 }
 
-// void rotasi(int a[MAX_VERTEX][MAX_VERTEX], int n, int centerX, int centerY) {
-//     int i;
-//     for (i = 0; i < n; i++) {
-//         a[i][0] = (int)((float)(a[i][0]-centerX)*sqrt(2.0)/2.0 - (float)(a[i][1]-centerY)*sqrt(2.0)/2.0) + centerX;
-//         a[i][1] = (int)((float)(a[i][0]-centerX)*sqrt(2.0)/2.0 + (float)(a[i][1]-centerY)*sqrt(2.0)/2.0) + centerY;
-//     }
-// }
+void dilatasi(int n, int c1, int c2, int c3, int factor, int centerX, int centerY, int *stillAvailable) {
+    int i;
+    for (i = 0; i < n; i++) {
+        if (copy_of_a[i][0]*factor - (factor-1)*centerX >= xResolution) {
+            *stillAvailable = 0;
+        } else {
+            a[i][0] = copy_of_a[i][0] * factor - (factor-1)*centerX;    
+        }
+        if (copy_of_a[i][1]*factor - (factor-1)*centerY >= yResolution) {
+            *stillAvailable = 0;
+        } else {
+            a[i][1] = copy_of_a[i][1] * factor - (factor-1)*centerY;
+        }
+    }
+}
 
-void rotasi(int centerX, int centerY, int dimension, int isX, int c1, int c2, int c3) {
-    if (isX%2 == 0) {
-        bresenham(centerX-dimension/2,centerY-dimension/2,centerX+dimension/2,centerY+dimension/2,0,0,0);
-        bresenham(centerX-dimension/2,centerY+dimension/2,centerX+dimension/2,centerY-dimension/2,0,0,0);
-        bresenham(centerX,centerY-dimension/2,centerX,centerY+dimension/2,c1,c2,c3);
-        bresenham(centerX-dimension/2,centerY,centerX+dimension/2,centerY,c1,c2,c3);
+void dilatasi_rotor(int n, int c1, int c2, int c3, int factor, int centerX, int centerY) {
+    int i;
+    for (i = 0; i < n; i++) {
+        rotor[i][0] = true_rotor[i][0] * factor - (factor-1)*centerX;    
+        copyrotor[i][0] = true_copyrotor[i][0] * factor - (factor-1)*centerX;
+        
+        rotor[i][1] = true_rotor[i][1] * factor - (factor-1)*centerY;
+        copyrotor[i][1] = true_copyrotor[i][1] * factor - (factor-1)*centerY;
     }
-    else {
-        bresenham(centerX,centerY-dimension/2,centerX,centerY+dimension/2,0,0,0);
-        bresenham(centerX-dimension/2,centerY,centerX+dimension/2,centerY,0,0,0);
-        bresenham(centerX-dimension/2,centerY-dimension/2,centerX+dimension/2,centerY+dimension/2,c1,c2,c3);
-        bresenham(centerX-dimension/2,centerY+dimension/2,centerX+dimension/2,centerY-dimension/2,c1,c2,c3);
+}
+
+void rotasi(int centerX, int centerY) {
+    int i;
+    for (i = 0; i < 4; i++) {
+        rotor[i][0] = (int)((float)(copyrotor[i][0]-centerX)*sqrt(2.0)/2.0 - (float)(copyrotor[i][1]-centerY)*sqrt(2.0)/2.0) + centerX;
+     	rotor[i][1] = (int)((float)(copyrotor[i][0]-centerX)*sqrt(2.0)/2.0 + (float)(copyrotor[i][1]-centerY)*sqrt(2.0)/2.0) + centerY;
+
+     	true_rotor[i][0] = (int)((float)(true_copyrotor[i][0]-centerX)*sqrt(2.0)/2.0 - (float)(true_copyrotor[i][1]-centerY)*sqrt(2.0)/2.0) + centerX;
+     	true_rotor[i][1] = (int)((float)(true_copyrotor[i][0]-centerX)*sqrt(2.0)/2.0 + (float)(true_copyrotor[i][1]-centerY)*sqrt(2.0)/2.0) + centerY;
     }
+}
+
+void initRotor(int centerX, int centerY, int dimension) {
+	copyrotor = (int **)malloc(4 * sizeof(int *));
+	true_copyrotor = (int **)malloc(4 * sizeof(int *));
+    for (int i=0; i<4; i++){
+         copyrotor[i] = (int *)malloc(2 * sizeof(int));
+         true_copyrotor[i] = (int *)malloc(2 * sizeof(int));
+    }
+
+    copyrotor[0][0] = centerX;
+    copyrotor[0][1] = centerY-dimension/2;
+    copyrotor[1][0] = centerX;
+    copyrotor[1][1] = centerY+dimension/2;
+    copyrotor[2][0] = centerX-dimension/2;
+    copyrotor[2][1] = centerY;
+    copyrotor[3][0] = centerX+dimension/2;
+    copyrotor[3][1] = centerY;
+
+    true_copyrotor[0][0] = centerX;
+    true_copyrotor[0][1] = centerY-dimension/2;
+    true_copyrotor[1][0] = centerX;
+    true_copyrotor[1][1] = centerY+dimension/2;
+    true_copyrotor[2][0] = centerX-dimension/2;
+    true_copyrotor[2][1] = centerY;
+    true_copyrotor[3][0] = centerX+dimension/2;
+    true_copyrotor[3][1] = centerY;
+
+    rotor = (int **)malloc(4 * sizeof(int *));
+    true_rotor = (int **)malloc(4 * sizeof(int *));
+    for (int i=0; i<4; i++){
+         rotor[i] = (int *)malloc(2 * sizeof(int));
+         true_rotor[i] = (int *)malloc(2 * sizeof(int));
+    }
+    rotasi(centerX,centerY);
+}
+
+void drawRotor(int c1, int c2, int c3, int count, int d) {
+	if (count%2 == 0) {
+		bresenham(copyrotor[0][0],copyrotor[0][1],copyrotor[1][0],copyrotor[1][1],c1,c2,c3);
+		bresenham(copyrotor[2][0],copyrotor[2][1],copyrotor[3][0],copyrotor[3][1],c1,c2,c3);
+		bresenham(copyrotor[0][0]+d,copyrotor[0][1],copyrotor[1][0]+d,copyrotor[1][1],c1,c2,c3);
+		bresenham(copyrotor[2][0]+d,copyrotor[2][1],copyrotor[3][0]+d,copyrotor[3][1],c1,c2,c3);
+	}
+	else {
+		bresenham(rotor[0][0],rotor[0][1],rotor[1][0],rotor[1][1],c1,c2,c3);
+		bresenham(rotor[2][0],rotor[2][1],rotor[3][0],rotor[3][1],c1,c2,c3);
+		bresenham(rotor[0][0]+d,rotor[0][1],rotor[1][0]+d,rotor[1][1],c1,c2,c3);
+		bresenham(rotor[2][0]+d,rotor[2][1],rotor[3][0]+d,rotor[3][1],c1,c2,c3);
+	}
 }
 
 void loadFile(char* filename, int *size){
@@ -218,9 +319,12 @@ void loadFile(char* filename, int *size){
     fscanf(fp, "%d", size);
 
     a = (int **)malloc((*size+1) * sizeof(int *));
+    copy_of_a = (int **)malloc((*size+1) * sizeof(int *));
     
-    for (int i=0; i<(*size+1); i++)
+    for (int i=0; i<(*size+1); i++){
          a[i] = (int *)malloc(2 * sizeof(int));
+     	 copy_of_a[i] = (int *)malloc(2 * sizeof(int));
+    }
 
     int i = 0;
     int j = 0;
@@ -229,23 +333,52 @@ void loadFile(char* filename, int *size){
     while(fscanf(fp, "%d", &temp)!=EOF){  
         if(j%2==0){
             a[i][0] = temp;
+            copy_of_a[i][0] = temp;
         }
         else{
             a[i][1] = temp;
+            copy_of_a[i][1] = temp;
             i++;
         }
         j++;  
     }
     
-    fclose(fp); 
+    fclose(fp);
+}
+
+void findCenter(int size, int *centerX, int *centerY){
+	int maxX = INT_MIN;
+    int minX = INT_MAX;
+
+    int maxY = INT_MIN;
+    int minY = INT_MAX;
+
+    for(int i=0; i<size; i++){
+    	if(maxX < a[i][0]){
+    		maxX = a[i][0];
+    	}
+    	if(minX > a[i][0]){
+    		minX = a[i][0];
+    	}
+
+    	if(maxY < a[i][1]){
+    		maxY = a[i][1];
+    	}
+    	if(minY > a[i][1]){
+    		minY = a[i][1];
+    	}
+    }
+
+    *centerX = minX + (maxX - minX)/2;
+    *centerY = minY + (maxY - minY)/2;
 }
 
 // Main
 int main(int argc, char **argv) {
     // Input resolution
     if (argc != 7) {
-        fprintf(stderr, "Please input filename resolution, red, green, and blue\n");
-        fprintf(stderr, "Example: %s 1366 768 255 255 255\n", argv[0]);
+        fprintf(stderr, "Please input filename, resolution, red, green, and blue\n");
+        fprintf(stderr, "Example: %s input.txt 1366 768 255 255 255\n", argv[0]);
         return -1;
     }
 
@@ -294,32 +427,55 @@ int main(int argc, char **argv) {
     int size;
 
     loadFile(filename, &size);
-    drawShape(size,255,255,255);
-    scanLine(size,blue,green,red);
 
-    // int stillAvailable = 1;
-    // int i;
+    int stillAvailable = 1;
+    
+    int centerX;
+    int centerY;
 
-    // usleep(1000000);
-    // for (;;) {
-    //     scanLine(a,4,0,0,0);
-    //     // dilatasi(a,4,blue,green,red,2,683,384,&stillAvailable);
-    //     rotasi(a,4,683,384);
-    //     if (stillAvailable == 1) {
-    //         scanLine(a,4,blue,green,red);
-    //     }
-    //     else {
-    //         break;
-    //     }
-    //     usleep(1000000);
-    // }
+    findCenter(size, &centerX, &centerY);
 
-    // int isX = 0;
-    // for (;;) {
-    //     rotasi(683,384,100,isX,blue,green,red);
-    //     usleep(150000);
-    //     isX++;
-    // }
+    int i = 1;
+    int distance_init = 160;
+    int distance = distance_init;
+
+    initRotor(592,351,26);
+
+    for (;;) {
+        dilatasi(size,blue,green,red,i,centerX,centerY,&stillAvailable);
+        dilatasi_rotor(4,blue,green,red,i,centerX, centerY);
+        distance = distance_init*i;
+
+        if (stillAvailable == 1) {
+            scanLine(size,blue,green,red);
+            
+            int count = 0;
+    		drawRotor(blue/2,green/2,red/2,count,distance);
+    		clock_t begin = clock();
+    		for(;;) {
+    			usleep(100000);
+    			drawRotor(255,255,255,count,distance);
+    			count++;
+    			drawRotor(blue/2,green/2,red/2,count,distance);
+    			clock_t end = clock();
+    			double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+    			if(time_spent >= 0.001){
+    				break;
+    			}
+    		}
+
+        	clear(size);
+        	drawRotor(0, 0, 0, 1, distance);
+        	drawRotor(0, 0, 0, 2, distance);
+        	i++;
+        }
+        else {
+            break;
+        }
+    }
+
+    
+    
 
     munmap(fbp, screensize);
     sleep(5);
